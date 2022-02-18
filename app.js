ef81eb9cd30c3eb8491d965f995346ec92bd1a57
fabiochidda <fabio.chidda99@gmail.com>
2022-02-18 02:29:36 +0100

    Sistemata la card creata da JS ed eliminata quella già presente nell'html

--- app.js
+++ app.js
@@ -44,13 +44,13 @@ for (i=0; i < team.length; i++) {
     <div class="team-card">
         <div class="card-image">
             <img
-                src="img/wayne-barnett-founder-ceo.jpg"
-                alt="Wayne Barnett"
+                src="img/${team[i].image}"
+                alt="${team[i].name}"
             />
         </div>
         <div class="card-text">
-            <h3>Wayne Barnett</h3>
-            <p>Founder & CEO</p>
+            <h3>${team[i].name}</h3>
+            <p>${team[i].role}</p>
         </div>
     </div>
 
